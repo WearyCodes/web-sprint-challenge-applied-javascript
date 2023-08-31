ee89c42e5b2a5fb86e0ab40f6afe58c18ab1fb7a
Sam Sanchez <wearycodes@gmail.com>
2023-08-30 23:50:37 -0500

    Cleaned up a little!

--- src/components/card.js
+++ src/components/card.js
@@ -26,27 +26,6 @@ articleHeadline.textContent = article.headline
 articlePhoto.src = article.authorPhoto
 articleAuthorName.textContent = article.authorName
 articleCard.addEventListener('click', ()=>{console.log(articleHeadline)})
-
-
-
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
   return articleCard
 }
 let articlestuff = []
@@ -79,26 +58,6 @@ spot.appendChild(Card(article.node[1]))
   
 }
   ).catch(err => 'error')
-
-// articlestuff.forEach(category => {
-//   category.forEach(article => {
-//     cards.push(Card(article))
-//   })
-// console.log(cards)
-// })
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5001/api/articles` (test it with console.log!!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-
-
-  
-
-
 
 }
 
